0e2efb13ecfc0eb7caba29660d0659ecab074fe5
Emanuel Castillo <emanuelcastillo9101@gmail.com>
2023-04-21 13:18:36 -0400

    refactor page

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -18,24 +18,21 @@ const CardList = ({
 }: TCardListProps): JSX.Element => {
 	return (
 		<View className="bg-white h-[400px] rounded-3xl py-4 px-1">
-			<ScrollView
-				showsHorizontalScrollIndicator={false}
-				showsVerticalScrollIndicator={false}>
-				{isLoading ? (
-					<SkeletonLoader />
-				) : data.length !== 0 ? (
-					<FlatList
-						className="flex-1"
-						data={data}
-						renderItem={({item}) => <CardItem item={item} onPress={onPress} />}
-						keyExtractor={item => String(item.id)}
-						showsVerticalScrollIndicator={false}
-						showsHorizontalScrollIndicator={false}
-					/>
-				) : (
-					<NotFound />
-				)}
-			</ScrollView>
+			{isLoading ? (
+				<SkeletonLoader />
+			) : data.length !== 0 ? (
+				<FlatList
+					className="flex-1"
+					data={data}
+					renderItem={({item}) => <CardItem item={item} onPress={onPress} />}
+					keyExtractor={item => String(item.id)}
+					showsVerticalScrollIndicator={false}
+					showsHorizontalScrollIndicator={false}
+					nestedScrollEnabled
+				/>
+			) : (
+				<NotFound />
+			)}
 		</View>
 	);
 };
